82f215d8f0986a16645b11b7aee4e34045c27703
Sean <s.cunningham777@gmail.com>
2023-01-02 15:35:15 -0600

    react ionic template

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'skyrim-potion-optimizer',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
