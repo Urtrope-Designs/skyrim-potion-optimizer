ba4ee4ad14e6685393c073295e76d7babebf60a0
Sean <s.cunningham777@gmail.com>
2023-02-22 15:40:45 -0600

    Remove checkboxes and buttons from recipes page

--- src/components/RecipeSummaryEntry.tsx
+++ src/components/RecipeSummaryEntry.tsx
@@ -1,14 +1,12 @@
-import { IonCheckbox, IonItem, IonLabel } from "@ionic/react";
+import { IonItem, IonLabel } from "@ionic/react";
 import { IRecipeSummaryViewmodel } from "../types/RecipeSummaryViewmodel";
 
 interface RecipeSummaryEntryProps {
   recipeSummary: IRecipeSummaryViewmodel;
-  updateRecipeSelection: (recipeIdToUpdate: number, isChecked: boolean) => void;
 }
 
-export const RecipeSummaryEntry: React.FC<RecipeSummaryEntryProps> = ({ recipeSummary, updateRecipeSelection }) => (
+export const RecipeSummaryEntry: React.FC<RecipeSummaryEntryProps> = ({ recipeSummary }) => (
   <IonItem>
-      <IonCheckbox slot="start" checked={recipeSummary.isSelected} onIonChange={(event) => updateRecipeSelection(recipeSummary.recipeId, event.detail.checked)}></IonCheckbox>
       <IonLabel>
         <h3>{recipeSummary.ingredientsList}</h3>
         {!!recipeSummary.standardEffectsList && <p>Effects: {recipeSummary.standardEffectsList}</p>}
--- src/pages/RecipesTab.tsx
+++ src/pages/RecipesTab.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonContent, IonItem, IonList, IonModal, IonPage } from '@ionic/react';
+import { IonContent, IonList, IonModal, IonPage } from '@ionic/react';
 import { useEffect, useRef, useState } from 'react';
 import { combineLatest } from 'rxjs';
 import { RecipeSummaryEntry } from '../components/RecipeSummaryEntry';
@@ -13,55 +13,25 @@ import './RecipesTab.css';
 export const RecipesTab: React.FC = () => {
   const settingsModal = useRef<HTMLIonModalElement>(null);
   const [recipeSummaries, setRecipeSummaries] = useState<IRecipeSummaryViewmodel[]>([]);
-  const [showOnlySelected, setShowOnlySelected] = useState<boolean>(false);
   const settingsToggleId = 'settings-toggle';
   
   useEffect(() => {
     combineLatest([dataManager.selectedRecipeIds$, dataManager.includedDLCIds$, dataManager.ingredientAvailabilityOptions$]).subscribe(([selectedRecipeIds, includedDLCIds, ingredientAvailabilityOptions]) => {
       const availableRecipes = recipeService.getAvailableRecipes(ALL_RECIPES, ALL_INGREDIENTS, includedDLCIds, ingredientAvailabilityOptions);
-      const recipeViewmodels = recipeService.getRecipeSummaryViewmodels(availableRecipes, ALL_INGREDIENTS, selectedRecipeIds);
+      const selectedRecipes = availableRecipes.filter(aR => selectedRecipeIds.includes(aR.id));
+      const recipeViewmodels = recipeService.getRecipeSummaryViewmodels(selectedRecipes, ALL_INGREDIENTS, selectedRecipeIds);
       setRecipeSummaries(recipeViewmodels);
     })
   }, []);
 
-  const toggleAllRecipes = () => {
-      if (recipeSummaries.every(rS => rS.isSelected)) {
-        dataManager.setSelectedRecipeIds([]);
-      } else {
-        dataManager.setSelectedRecipeIds(recipeSummaries.map(rS => rS.recipeId));
-      }
-  }
-  
   return (
     <IonPage>
       <StandardHeader title="Potion Recipes" settingsToggleBtnId={settingsToggleId}></StandardHeader>
       <IonContent fullscreen>
         <IonList>
-          <IonItem>
-            <IonButtons slot="start">
-              <IonButton
-                color="primary"
-                fill={recipeSummaries.every(rS => rS.isSelected) ? 'outline' : 'solid'}
-                onClick={() => toggleAllRecipes()}
-              >
-                {recipeSummaries.every(rS => rS.isSelected) ? 'Deselect All' : 'Select All'}
-              </IonButton>
-              <IonButton
-                color="primary"
-                fill={showOnlySelected ? 'outline' : 'solid'}
-                onClick={() => setShowOnlySelected(!showOnlySelected)}
-              >
-                {showOnlySelected ? 'Show All' : 'Hide Deselected'}
-              </IonButton>
-            </IonButtons>
-          </IonItem>
           {
-            recipeSummaries
-            .filter(recipe => {
-              return !showOnlySelected || recipe.isSelected;
-            })
-            .map(recipe => {
-              return <RecipeSummaryEntry key={recipe.recipeId} recipeSummary={recipe} updateRecipeSelection={dataManager.updateRecipeSelection}></RecipeSummaryEntry>
+            recipeSummaries.map(recipe => {
+              return <RecipeSummaryEntry key={recipe.recipeId} recipeSummary={recipe}></RecipeSummaryEntry>
             })
           }
         </IonList> 
